15f67ea43a05be3c29f773ee3e7185c06b2cea6e
Gopika Ghosh <gopikaghosh167@gmail.com>
2025-08-21 14:56:48 -0300

    iterate over arrays and objects

---
+++ arrayandobjects_iteration.js
@@ -0,0 +1,29 @@
+// Task 1
+var dairy = ["cheese", "sour cream", "milk", "yogurt", "ice cream", "milkshake"];
+function logDiary() {
+    for (const item of dairy) {
+        console.log(item);
+    }
+}
+logDiary();
+// Task 2
+const animal = {
+    canJump: true
+};
+
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+function birdCan() {
+    for (const key of Object.keys(bird)) {
+        console.log(`${key}: ${animal[key]}`);
+    }
+}
+birdCan();
+// Task 3
+function animalCan() {
+    for (const key in bird) {
+        console.log(`${key} : ${bird[key]}`);
+    }
+}
+animalCan();
